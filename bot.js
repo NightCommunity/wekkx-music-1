d0cd6c250a6b932c2431759478e557113f8bd346
NightCommunity <42852008+NightCommunity@users.noreply.github.com>
2018-09-12 19:06:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,33 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '1'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Night`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+const Discord = require('discord.js');
 
 const Util = require('discord.js');
 
@@ -20,7 +49,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "!";
+const prefix = "1";
 /////////////////////////
 ////////////////////////
 
@@ -342,4 +371,5 @@ client.on('message', message => {
     }
 });
 
+client.login(process.env.BOT_TOKEN);
 client.login(process.env.BOT_TOKEN);
